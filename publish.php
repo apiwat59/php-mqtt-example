81d0e2ef972960d25e3a39ed6c88638e81ce507e
apiwat59 <32583026+apiwat59@users.noreply.github.com>
2019-02-21 10:17:39 +0700

    Update publish.php

--- publish.php
+++ publish.php
@@ -1,14 +1,14 @@
 <?php
 require('vendor/autoload.php');
 
-$url = parse_url(getenv('CLOUDMQTT_URL'));
-$topic = substr($url['path'], 1);
+$url = parse_url(getenv('m16.cloudmqtt.com'));
+$topic = substr($url['/ESP/realy'], 1);
 $client_id = "phpMQTT-publisher";
 
 $message = "Hello CloudMQTT!";
 
-$mqtt = new Bluerhinos\phpMQTT($url['host'], $url['port'], $client_id);
-if ($mqtt->connect(true, NULL, $url['user'], $url['pass'])) {
+$mqtt = new Bluerhinos\phpMQTT($url['m16.cloudmqtt.com'], $url['17495'], $client_id);
+if ($mqtt->connect(true, NULL, $url['cszwalpv'], $url['Gqxx2jCFxwsR'])) {
     $mqtt->publish($topic, $message, 0);
     echo "Published message: " . $message;
     $mqtt->close();
